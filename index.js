5ba3a22314999d1ab382361d5613f1d81fca82f4
Rafael Sanches <fucazu@gmail.com>
2018-02-12 16:02:10 -0200

    Testando build com TravisCI

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+const fs = require('fs')
+const { resolve, join } = require('path')
+const shell = require('shelljs')
+const indexFile = 'index.js'
+
+const createFolder = folder => fs.mkdirSync(folder)
+const createFile = filePath => fs.writeFileSync(filePath, '')
+const copyFiles = distFolderPath => fs.copyFileSync(resolve(join(__dirname, './gitignore')), resolve(join(distFolderPath, './.gitignore')))
+
 module.exports = folder => {
-  const fs = require('fs')
-  const { resolve, join } = require('path')
-  const shell = require('shelljs');
-  const indexFile = 'index.js'
-
-  const createFolder = folder => fs.mkdirSync(folder)
-  const createFile = filePath => fs.writeFileSync(filePath, '')
-  const copyFiles = distFolderPath => fs.copyFileSync(resolve(join(__dirname,'./gitignore')), resolve(join(distFolderPath, './.gitignore')))
-
   folder = resolve(folder)
 
   !fs.existsSync(folder) && createFolder(folder)
